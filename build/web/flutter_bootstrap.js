6859a92d2d734d7bddc7b67d154065ac53940521
Rajan Gurung <bardangrg0@gmail.com>
2025-03-02 11:00:55 -0500

    portfolio

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3422717897"
+    serviceWorkerVersion: "453228144"
   }
 });
